75475b3ffca4b5e215fa386ecbe0296f894481ba
gorisgor <igor.matvienko1989@gmail.com>
2024-06-11 20:01:38 +0300

    Update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,27 @@
-export default function ({name, tag, location, avatar, stats}){
-    return(
-        <div>
-          <div>
-            <img
-              src={avatar}
-              alt="User avatar"
-            />
-            <p>{name}</p>
-            <p>@{tag}</p>
-            <p>{location}</p>
-          </div>
-        
-          <ul>
-            <li>
-              <span>Followers</span>
-              <span>{stats.followers}</span>
-            </li>
-            <li>
-              <span>Views</span>
-              <span>{stats.views}</span>
-            </li>
-            <li>
-              <span>Likes</span>
-              <span>{stats.likes}</span>
-            </li>
-          </ul>
-        </div>
-        
-    )
+import css from "./Profile.module.css";
+export default function Profile({ name, tag, location, avatar, stats }) {
+  return (
+    <div className={css.profileCard}>
+      <div>
+        <img src={avatar} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
