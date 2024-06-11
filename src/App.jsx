cb461ac227bfa96cb6ddaac7d4c327311a9eec33
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-06-11 08:20:55 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Profile from "./components/Profile/Profile";
 import userData from "./userData.json";
 import FriendList from "./components/FriendList/FriendList";
 import friends from "./friends.json"
+import transactions from './transactions.json'
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 export default function App (){
     return (
-        <>
-        <Profile userData = {userData} />
-        <FriendList friends = {friends}/>
-        </>
+        <div>
+          <Profile userData = {userData} />
+          <FriendList friends = {friends}/>
+          <TransactionHistory items={transactions} />
+        </div>
     )
 }
