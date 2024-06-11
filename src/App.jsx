26a0310d84f457177d8cfb3eed9b49192bb7c18e
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-06-11 07:21:32 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Profile from "./components/profile/profile";
+import Profile from "./components/Profile/Profile";
 import userData from "./userData.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./friends.json"
 
 export default function App (){
     return (
         <>
         <Profile userData = {userData} />
+        <FriendList friends = {friends}/>
         </>
     )
 }
