489e76e5fcdfec203e0f2dfbdc3a3d0bc0b88717
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-06-12 09:39:43 +0000

    StylesProfile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,27 @@
-import css from "./Profile.module.css";
+import React from 'react';
+import css from './Profile.module.css';
+
 export default function Profile({ name, tag, location, avatar, stats }) {
   return (
     <div className={css.profileCard}>
       <div>
-        <img src={avatar} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+        <img src={avatar} alt="User avatar" className={css.profileImage} />
+        <p className={css.profileName}>{name}</p>
+        <p className={css.profileTag}>@{tag}</p>
+        <p className={css.profileLocation}>{location}</p>
       </div>
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.profileStats}>
+        <li className={css.profileStatItem}>
+          <span className={css.profileStatLabel}>Followers</span>
+          <span className={css.profileStatValue}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.profileStatItem}>
+          <span className={css.profileStatLabel}>Views</span>
+          <span className={css.profileStatValue}>{stats.views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.profileStatItem}>
+          <span className={css.profileStatLabel}>Likes</span>
+          <span className={css.profileStatValue}>{stats.likes}</span>
         </li>
       </ul>
     </div>
